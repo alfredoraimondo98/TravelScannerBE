1b405e4b27f38fc748408377b2f8ad6e74ab7690
alfredoraimondo98 <alfredoraimondo98@gmail.com>
2021-12-04 15:05:28 +0100

    Bcrypts, Cors, Express-Validator module

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,5 @@
 const database = require('../utils/database');
 const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
 const { validationResult } = require('express-validator');
 const query = require('../utils/queries')
 
